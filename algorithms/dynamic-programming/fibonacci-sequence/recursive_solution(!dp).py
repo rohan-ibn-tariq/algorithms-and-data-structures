140a52745baaea9da0e26d6adae6b520b9dc7744
rohan-ibn-tariq <178284954+rohan-ibn-tariq@users.noreply.github.com>
2025-08-04 12:36:14 +0500

    bais non dynamic programming based recursive fibonacci sequence solution

---
+++ algorithms/dynamic-programming/fibonacci-sequence/recursive_solution(!dp).py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Filename:    recursive_solution(!dp).py
+Author:      Muhammad Rohan Ali ibn Tariq Asmat
+Created on:  2025-08-04
+Description: A plain recursive solution to the fibonacci sequence.
+
+Usage:
+    python recursive_solution(!dp).py
+
+Dependencies:
+    - None
+
+License: As of Repository License
+"""
+
+def fibonacci(n: int) -> int: 
+    """
+    Calculate the nth Fibonacci number using a plain recursive approach.
+    """
+
+    if n<=1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+if __name__ == "__main__":
+    n = 5
+    # Should print: 5
+    print(f"Fibonacci number at position {n} is: {fibonacci(n)}")
+
+    n = 10
+    # Should print: 55
+    print(f"Fibonacci number at position {n} is: {fibonacci(n)}")

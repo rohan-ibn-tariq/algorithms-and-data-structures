213d25fad0247369109d41bdc1b2fb7f75fb9e80
rohan-ibn-tariq <178284954+rohan-ibn-tariq@users.noreply.github.com>
2025-08-04 16:28:38 +0500

    added space efficient dynamic programming approach for fibonacci sequence.

---
+++ algorithms/fibonacci-sequence/dynamic-programming/space_efficient_tabulation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Filename:    space_efficient_tabulation.py
+Author:      Muhammad Rohan Ali ibn Tariq Asmat
+Created on:  2025-08-04
+Description: A space-efficient dynamic programming approach to the fibonacci sequence.
+
+Usage:
+    python space_efficient_tabulation.py
+
+Dependencies:
+    - None
+
+License: As of Repository License
+"""
+
+def fibonacci(n: int) -> int: 
+    """
+    Calculate the nth Fibonacci number using space efficient tabulation approach.
+    """
+    current = 0
+    previous = 1
+    previous_previous = 0
+
+    if n == 0:
+        return previous_previous
+    elif n == 1:
+        return previous
+
+    for i in range(2, n + 1):
+        current = previous + previous_previous
+        previous_previous = previous
+        previous = current
+
+    return current
+
+
+if __name__ == "__main__":
+    n = 5
+    # Should print: 5
+    print(f"Fibonacci number at position {n} is: {fibonacci(n)}")
+
+    n = 10
+    # Should print: 55
+    print(f"Fibonacci number at position {n} is: {fibonacci(n)}")

9812c9c8c7f09a16fb05aa3263c7f4ffae811cd2
rohan-ibn-tariq <your-username@users.noreply.github.com>
2025-08-04 15:07:31 +0500

    added dynamic programming tabulation approach for fibonacci sequence

---
+++ algorithms/dynamic-programming/fibonacci-sequence/tabulation_approach.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Filename:    tabulation_approach.py
+Author:      Muhammad Rohan Ali ibn Tariq Asmat
+Created on:  2025-08-04
+Description: A tabulation approach to the fibonacci sequence.
+
+Usage:
+    python tabulation_approach.py
+
+Dependencies:
+    - None
+
+License: As of Repository License
+"""
+
+def fibonacci(n: int) -> int: 
+    """
+    Calculate the nth Fibonacci number using a tabulation approach.
+    """
+    if n <= 1:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+if __name__ == "__main__":
+    n = 5
+    # Should print: 5
+    print(f"Fibonacci number at position {n} is: {fibonacci(n)}")
+
+    n = 10
+    # Should print: 55
+    print(f"Fibonacci number at position {n} is: {fibonacci(n)}")
